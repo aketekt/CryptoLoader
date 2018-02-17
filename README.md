c4a3c28f3687e9a7c4b4795cf0230af801b6dde3
Will <will@Will.Dev>
2018-02-17 22:06:46 +0000

    log outputs

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-Simpe Crypto Downloader:
+Simple crypto data downloader
 
+INSTALL
 
+`npm install -g`
+`which cryptoloader`
+......../usr/local/bin/cryptoloader
+`readlink ....../usr/local/bin/cryptoloader`
+..
+`npm link`
--- index.js
+++ index.js
@@ -1,177 +1,113 @@
 #!/usr/bin/env node
 
 "use strict";
-
-const wrapper = require("./");
 const json2csv = require('json2csv');
 const fs = require('fs');
-const jsonfile = require('jsonfile');
+const wrapper = require("./");
 const co = require('co');
 const prompt = require('co-prompt');
 const program = require('commander');
 const chalk = require('chalk');
-var result;
+var FormData = require('form-data');
 
 
-
-
-
-
-// BINANCE CMD
+// CMD
 program
-    .command('binance <base> <quote> <startTime> <endTime> <resolution>')
-    .action(function(base, quote, startTime, endTime,resolution) 
+    .arguments('<exchange>')
+    .arguments('<base>')
+    .arguments('<quote>')
+    .arguments('<startTime>')
+    .arguments('<endTime>')
+    .arguments('<resolution>')
+    .action(function(exchange, base, quote, startTime, endTime,resolution) 
     {
-        //Create public API wrapperSet + CSV data fields
-        const request = new wrapper.binance();
-        const fields = ['0', '1', '2', '3', '4', '5', '6','7','8','9','10', '11'];
-        
-        //format inputs
-        var startTS = new Date(startTime).getTime() ;
-        console.log(startTS);
-        var endTS = new Date(endTime).getTime() ;
-        console.log(endTS);
-        var pair =  quote + base;
-        var fileName = base + quote + '-' +startTS +'-'+ endTS+'.csv';
-        
-        var ResolutionInSeconds = 0;
-        var dataFile = [];
+        switch(exchange){
+            case 'binance':
+                var startTS = new Date(startTime).getTime();
+                var endTS = new Date(endTime).getTime();
+                var pair =  quote + base;
+                var PUBLIC_URL = "https://api.binance.com//api/v1/klines";
+                var maxDatapointsPerRequest = 500;
+                var resolutionInSeconds = 0;
 
-       
-        switch(resolution) 
-        {
-            case 'minute':  
-            resolution = "1m";
-            ResolutionInSeconds = 60000;
+                //const fields = ['symbol', 'date', 'open', 'high', 'low', 'close', 'volume'];
+                switch(resolution) 
+                {
+                    case 'minute':  
+                        resolution = "1m";
+                        resolutionInSeconds = 60000;
+                        break;
+                    case 'hour': 
+                        resolution = "1h";
+                        resolutionInSeconds = 3600000;
+                        break;
+                    case 'day': 
+                        resolution = "1d";
+                        resolutionInSeconds = 86400000;
+                }
+                var params = {symbol: pair, interval: resolution, startTime: startTS, endTime: endTS}
             break;
-            case 'hour': 
-            resolution = "1h";
-            ResolutionInSeconds = 3600000;
-            break;
-            case 'day': 
-            resolution = "1d";
-            ResolutionInSeconds = 86400000;
-            break;
+            case 'poloniex':
+                var startTS = new Date(startTime).getTime() / 1000;
+                var endTS = new Date(endTime).getTime() / 1000;
+                var pair = base + '_' + quote;
+                var PUBLIC_URL = 'https://poloniex.com/public';
+                var requestCommand = 'returnChartData';
+                var maxDatapointsPerRequest = 200000;
+                var resolutionInSeconds = 0;
+                    
+                switch(resolution) 
+                {
+                    case 'minute':  
+                        resolution = 300;
+                        resolutionInSeconds = resolution;
+                        console.log(chalk.yellow("Warning: API limit, Minimum is 5m resolution, Downloading 5m..."));
+                    break;
+                    case 'hour': 
+                        resolution = 1800;
+                        resolutionInSeconds = resolution;
+                        console.log(chalk.yellow("Warning: API limit, Resolution cannot be 1h, Downloading 30m..."));
+                    break;
+                    case 'day': 
+                        resolution = 86400;
+                        resolutionInSeconds = resolution;
+                }
+                                        
         }
 
-
         // Set Params for API request
-        var params = {symbol: pair, interval: resolution, startTime: startTS, endTime: endTS}
-
-
-        //variables for loop
-        var maxDatapointsPerRequest = 500;
-        var results;
-        var windowEnd = endTS;
-     
-
+        const request = new wrapper.publicAPI(PUBLIC_URL, exchange);
+        var JSONfileName = base + quote + '-' +startTS +'-'+ endTS + '.json';
+        var CSVfileName = base + quote + '-' +startTS +'-'+ endTS + '.csv';
+        
+        //Loop and create request blocks
+        var windowEnd = endTS; 
         function loop(){
             setTimeout(function(){
-                if(startTS + (ResolutionInSeconds * maxDatapointsPerRequest) > windowEnd)
+                if(startTS + (resolutionInSeconds * maxDatapointsPerRequest) > windowEnd)
             {
             endTS = windowEnd;
             } else 
             {
-            endTS = startTS + (ResolutionInSeconds * maxDatapointsPerRequest); 
+            endTS = startTS + (resolutionInSeconds * maxDatapointsPerRequest); 
             };
-            params = {symbol: pair, interval: resolution, startTime: startTS, endTime: endTS};
+            params = {command: requestCommand, symbol: pair, interval: resolution, startTime: startTS, endTime: endTS}
                 request.returnChartData(params,(err, response) => 
-                {            
-                    response.forEach(function(item) {
-                        dataFile.push(item)
-                    });
-                    var result = json2csv({ data: dataFile, fields: fields });
-                    fs.writeFile(fileName, result, function(err) 
-                    {
-                        if (err) throw err;
-                        console.log(chalk.green('file saved as: ')+ fileName);
-            });    
+                
+                {  
+                    if (err) {
+                        throw err.msg;
+                    }
+                    else console.log('response recieved'); 
+                  
+                    console.log(response);
                     if(startTS != windowEnd){
                         loop();
                     }
-                    
-                },1000);
+                },0);
             startTS = endTS; 
             })
         }
-        loop();
-        
-        
-            
-           
-        
-
-         
-            
-        
-        //result = json2csv({ data: result, fields: fields });
-       
-              
-        
+loop();
     });  
-           
-
-// POLONIEX CMD
-program
-    .command('poloniex <base> <quote> <startTime> <endTime> <resolution>')
-    .action(function(base, quote, startTime, endTime,resolution) 
-    {
-        const request = new wrapper.poloniex();
-        const fields = ['symbol', 'date', 'open', 'high', 'low', 'close', 'volume'];
-        
-        // Format inputs
-        var startTS = new Date(startTime).getTime() / 1000;
-        var endTS = new Date(endTime).getTime() / 1000; //repeated code
-        var fileName = base + quote + '_' + startTS + '_' + endTS + '.csv';
-        var pair = base + '_' + quote;
-
-        switch(resolution) 
-        {
-            case 'minute':  
-            resolution = 300;
-            console.log(chalk.yellow("Warning: API limit, Minimum is 5m resolution, Downloading 5m..."));
-            break;
-            case 'hour': 
-            resolution = 1800;
-            console.log(chalk.yellow("Warning: API limit, Resolution cannot be 1h, Downloading 30m..."));
-            break;
-            case '2hour': 
-            resolution = 7200;
-            break;
-            case 'day': 
-            resolution = 86400;
-            break;
-        }
-
-        var params = {currencyPair: pair, start: startTS, end: endTS, period: resolution};
-
-        // Loop here -->
-
-        // Send API request and write response to CSV
-        request.returnChartData(params,(err, response) => {
-            if (err) 
-            {
-                throw err.msg;
-            } 
-            else console.log('Response: 200');
-            response.fill('symbol',pair);
-
-            try 
-            {
-                var result = json2csv({ data: response, fields: fields });
-            } 
-            catch (err) 
-            {
-                // Errors are thrown for bad options, or if the data is empty and no fields are provided.
-                // Be sure to provide fields if it is possible that your data array will be empty.
-                console.error(err);
-            }
-
-            fs.writeFile(fileName, result, function(err) 
-            {
-                if (err) throw err;
-                console.log(chalk.green('file saved as: ')+ fileName);
-            });            
-        });  
-    });
 program.parse(process.argv);
