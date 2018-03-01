8aab6a08cdd519d55e5a771b2af75fa6d34b1f2e
Will <will@Will.Dev>
2018-03-01 13:36:59 +0000

    add bitfinex & add ALL to CLI

--- index.js
+++ index.js
@@ -11,140 +11,60 @@ const program = require('commander');
 const chalk = require('chalk');
 const dataFormat = require("./lib/common/dataFormat.js");
 
+//data
 let data = []; 
 var dataToSave = [];
 
-//
-// MULTI PAIR LOOP PARAMS
-//
+var pairList =  ['ZECBTC', 'DASHBTC', 'XRPBTC', 'XMRBTC', 'SDCBTC', 'GNTBTC', 'GNTETH', 'GASBTC', 'GNOBTC', 'GNOETH', 'DGBBTC', 'AMPBTC', 'FCTBTC', 'REPBTC', 'REPETH', 'NXTBTC', 'DOGEBTC', 'MAIDBTC', 'SYSBTC', 'LTCBTC', 'LSKBTC', 'XEMBTC', 'STEEMBTC', 'ETCBTC', 'ETCETH', 'NAVBTC', 'SCBTC', 'BTCUSDT', 'ETHUSDT', 'XMRUSDT', 'DASHUSDT', 'ZECUSDT', 'STRBTC', 'POTBTC', 'GAMEBTC', 'EXPBTC', 'STRATBTC', 'PASCBTC', 'DCRBTC', 'VTCBTC', 'BTSBTC', 'ETCUSDT', 'LTCUSDT', 'XRPUSDT', 'ARDRBTC', 'BURSTBTC', 'ZRXBTC', 'CVCBTC', 'OMGBTC' ];
 
-var pairList = 
-[
-    "BTC_BCN", 
-    "BTC_BELA", 
-    "BTC_BLK", 
-    "BTC_BTCD", 
-    "BTC_BTM", 
-    "BTC_BTS", 
-    "BTC_BURST", 
-    "BTC_CLAM", 
-    "BTC_DASH", 
-    "BTC_DGB", 
-    "BTC_DOGE", 
-    "BTC_EMC2", 
-    "BTC_FLDC", 
-    "BTC_FLO", 
-    "BTC_GAME", 
-    "BTC_GRC", 
-    "BTC_HUC", 
-    "BTC_LTC", 
-    "BTC_MAID", 
-    "BTC_OMNI", 
-    "BTC_NAV", 
-    "BTC_NEOS", 
-    "BTC_NMC", 
-    "BTC_NXT", 
-    "BTC_PINK", 
-    "BTC_POT", 
-    "BTC_PPC", 
-    "BTC_RIC", 
-    "BTC_STR", 
-    "BTC_SYS", 
-    "BTC_VIA", 
-    "BTC_XVC", 
-    "BTC_VRC", 
-    "BTC_VTC", 
-    "BTC_XBC", 
-    "BTC_XCP", 
-    "BTC_XEM", 
-    "BTC_XMR", 
-    "BTC_XPM", 
-    "BTC_XRP", 
-    "USDT_BTC", 
-    "USDT_DASH", 
-    "USDT_LTC", 
-    "USDT_NXT", 
-    "USDT_STR", 
-    "USDT_XMR", 
-    "USDT_XRP", 
-    "XMR_BCN", 
-    "XMR_BLK", 
-    "XMR_BTCD", 
-    "XMR_DASH", 
-    "XMR_LTC", 
-    "XMR_MAID", 
-    "XMR_NXT", 
-    "BTC_ETH", 
-    "USDT_ETH", 
-    "BTC_SC", 
-    "BTC_BCY", 
-    "BTC_EXP", 
-    "BTC_FCT", 
-    "BTC_RADS", 
-    "BTC_AMP", 
-    "BTC_DCR", 
-    "BTC_LSK", 
-    "ETH_LSK", 
-    "BTC_LBC", 
-    "BTC_STEEM", 
-    "ETH_STEEM", 
-    "BTC_SBD", 
-    "BTC_ETC", 
-    "ETH_ETC", 
-    "USDT_ETC", 
-    "BTC_REP", 
-    "USDT_REP", 
-    "ETH_REP", 
-    "BTC_ARDR", 
-    "BTC_ZEC", 
-    "ETH_ZEC", 
-    "USDT_ZEC", 
-    "XMR_ZEC", 
-    "BTC_STRAT", 
-    "BTC_NXC", 
-    "BTC_PASC", 
-    "BTC_GNT", 
-    "ETH_GNT", 
-    "BTC_GNO", 
-    "ETH_GNO", 
-    "BTC_BCH", 
-    "ETH_BCH", 
-    "USDT_BCH", 
-    "BTC_ZRX", 
-    "ETH_ZRX", 
-    "BTC_CVC", 
-    "ETH_CVC", 
-    "BTC_OMG", 
-    "ETH_OMG", 
-    "BTC_GAS", 
-    "ETH_GAS", 
-    "BTC_STORJ"
-];
-    
-        var pairPosition = 0;
-        var pairsMax = 98;
-    
 
-//
-// MULTI PAIR LOOP PARAMS
-//
+var pairPosition = 0;
+var pairsMax = 105;
+var pairName;
 
 // CMD
 program
     .arguments('<exchange>')
-    .arguments('<base>')
-    .arguments('<quote>')
+    .arguments('<instrument>')
     .arguments('<startTime>')
     .arguments('<endTime>')
     .arguments('<resolution>')
-    .action(function(exchange, base, quote, startTime, endTime, resolution) 
+    .action(function(exchange, instrument, startTime, endTime, resolution) 
     {
+
+        //Loop vars
+        var startTS = new Date(startTime).getTime();
+        var endTS = new Date(endTime).getTime();
+        var resolutionInSeconds = 0;
+        // ALL loop vars
+      
+
         switch(exchange)
         {
+            case 'bitfinex':
+                var x = instrument.split("-");
+                var pair = x[1] + x[0];
+                var maxDatapointsPerRequest = 119;
+                var PUBLIC_URL;
+
+            switch(resolution) 
+            {
+                case 'minute':  
+                    resolution = "1m";
+                    resolutionInSeconds = 60000;
+                    break;
+                case 'hour': 
+                    resolution = "1h";
+                    resolutionInSeconds = 3600000;
+                    break;
+                case 'day': 
+                    resolution = "1D";
+                    resolutionInSeconds = 86400000;
+            };
+        break;
             case 'binance':
-                var startTS = new Date(startTime).getTime();
-                var endTS = new Date(endTime).getTime();
-                var pair =  quote + base;
+                var x = instrument.split("-");
+                var pair = x[1] + x[0];
                 var PUBLIC_URL = "https://api.binance.com/api/v1/klines";
                 var maxDatapointsPerRequest = 500;
                 var resolutionInSeconds = 0;
@@ -167,7 +87,7 @@ program
             case 'poloniex':
                 var startTS = new Date(startTime).getTime() / 1000;
                 var endTS = new Date(endTime).getTime() / 1000;
-                var pair = base + '_' + quote;
+                var pair =  instrument.replace('-', '_');
                 var PUBLIC_URL = 'https://poloniex.com/public';
                 var requestCommand = 'returnChartData';
                 var maxDatapointsPerRequest = 10000;
@@ -191,96 +111,141 @@ program
                 };                                         
             };
         
-        // Set Params for request loop & create file names
-        const request = new wrapper.publicAPI(PUBLIC_URL, exchange);
-        var CSVfileName = "error" + '.csv';
-       
-   
-        //Loop and create request blocks
-        var windowEnd = endTS;
-        var windowStart = startTS;
 
-        (function loop(callback){
-            setTimeout(function()
-            {
+        var CSVfileName = CSVfileName = pair + '-' +startTime +'-'+ endTime + '.csv';
+        
+         //Loop and create request blocks
+         var windowEnd = endTS;
+         var windowStart = startTS;
+ 
+         (function loop(){
+             setTimeout(function()
+             {
+                if (exchange == 'bitfinex') PUBLIC_URL = "https://api.bitfinex.com/v2/candles/trade:"+ resolution +":"+"t"+ pair +"/hist";
+                const request = new wrapper.publicAPI(PUBLIC_URL, exchange);
+
                 //Prevent loop requesting data after windowEnd
                 if(startTS + (resolutionInSeconds * maxDatapointsPerRequest) > windowEnd)
                 {
                     endTS = windowEnd;
                 } else endTS = startTS + (resolutionInSeconds * maxDatapointsPerRequest); 
-             
-                var pair = pairList[pairPosition];
-                var params = {
-                                command: requestCommand, 
-                                symbol: pair, 
-                                interval: resolution, 
-                                startTime: startTS, 
-                                endTime: endTS
+                if(instrument == 'ALL') pair = pairList[pairPosition];
+
+                var params ={
+                                 command: requestCommand, 
+                                 symbol: pair, 
+                                 interval: resolution, 
+                                 startTime: startTS, 
+                                 endTime: endTS
                             };
-                          
-                //console.log(chalk.redBright('REQUEST FAILED: Retrying ' + cntr + ' of ' + retryTimes + 'times...' ));
-                          
-                request.returnChartData(params,(err, response) =>             
-                {   
-                    if (err) {
-                        throw err.msg;
-                    }   else console.log(chalk.cyan('     RESPONSE: ')+'200');
-
-                    //Log some human readable dates for each request. 
-                    if(exchange != 'poloniex') {
-                        console.log(chalk.cyan('     REQUEST BLOCK: ')+ new Date(startTS) + ' - ' + new Date(endTS));          
-                    }   else console.log(chalk.cyan('     REQUEST BLOCK: ')+ new Date((startTS *1000)) + ' - ' + new Date((endTS * 1000))); 
-                   
+    
+                           
+                 //console.log(chalk.redBright('REQUEST FAILED: Retrying ' + cntr + ' of ' + retryTimes + 'times...' ));
+                           
+                 request.returnChartData(params,(err, response) =>             
+                 {   
+                     if (err) {
+                         throw err.msg;
+                     }   else console.log(chalk.cyan('     RESPONSE: ')+'200');
+ 
+                     //Log some human readable dates for each request. 
+                     if(exchange != 'poloniex') {
+                         console.log(chalk.cyan('     REQUEST BLOCK: ')+ new Date(startTS) + ' - ' + new Date(endTS));          
+                     }   else console.log(chalk.cyan('     REQUEST BLOCK: ')+ new Date((startTS *1000)) + ' - ' + new Date((endTS * 1000))); 
                     
-                    response.forEach(function(item) {
-                        data.push(item)  
-                    });
-                    if (err) {
-                        throw err.msg;
-                    }   else console.log(chalk.cyan('     DATA: ') + 'Recieved');
-                                   
-                    startTS = endTS;
-                    if(startTS == windowEnd)
+                     
+                     response.forEach(function(item) {
+                         data.push(item)  
+                     });
+                     if (err) {
+                         throw err.msg;
+                     }   else console.log(chalk.cyan('     DATA: ') + 'Recieved');
+                                    
+                     startTS = endTS;
+                     if(instrument == 'ALL') {
+                        if (pairPosition <= pairsMax) loop();
+                        if(startTS == windowEnd)
+                        {
+                            startTS = windowStart;
+                            pairName = pair;
+                            CSVfileName = pairName + '-' +startTime +'-'+ endTime + '.csv';
+                            pairPosition++;
+                            dataToSave = data;
+                            parseData();
+                            data = [];
+                        }
+                        
+                    } else 
                     {
-                        startTS = windowStart;
-                        var pairName = pair;
-                        CSVfileName = pairName + '-' +startTime +'-'+ endTime + '.csv';
-                        parseData();
-                        pairPosition++;
-                        dataToSave = data;
-                        data = [];
-                    } 
+                        if(startTS != windowEnd) loop(); else 
+                        {    
+                            pairName = pair;
+                            dataToSave = data;
+                            parseData();
+                        }
+                       
+                    }
+                 });          
+             },750);
+         })();
 
-                    if (pairPosition <= pairsMax) loop();
-                    
-                });          
-            },1000);
-        })();
+
+        
+           
+
+     
       
     //Parse the data and edit it
     function parseData()
     {
         switch(exchange) 
         {
-            case 'binance':
+            case 'bitfinex':
                 console.log(chalk.cyan('     DATA: ')+ 'Parsing data...');
-               
                     //Format dates, times and add symbol  
-                    data.forEach(function(element) 
+                    dataToSave.forEach(function(element) 
                     {
+                        element[6] = element[0];
                         let openTime = new Date(element[0]);
-                        let closeTime = new Date(element[6]);
-                        element[0] = dataFormat.formatTime(openTime);      
-                        element[6] = dataFormat.formatTime(closeTime);
-                        element[11] = dataFormat.formatDate(openTime); 
-                        element.symbol = base+quote;        
+                        element[0] = dataFormat.formatDate(openTime) + ' ' + dataFormat.formatTime(openTime);      
+                        element.symbol = pairName;    
                     });               
                
                     // Update dataPoint keys
-                    var dataList = data.map(function(dataPoint)
+                    var dataList = dataToSave.map(function(dataPoint)
                     { 
                         return { 
-                            openTime: dataPoint[0], 
+                            time: dataPoint[0], 
+                            open: dataPoint[1],
+                            close: dataPoint[2],
+                            high: dataPoint[3],
+                            low: dataPoint[4],
+                            volume: dataPoint[5],
+                            TS: dataPoint[6],
+                            symbol: dataPoint.symbol        
+  
+                        };              
+                    });                
+            break;
+            case 'binance':
+                console.log(chalk.cyan('     DATA: ')+ 'Parsing data...');
+        
+                    //Format dates, times and add symbol  
+                    dataToSave.forEach(function(element) 
+                    {
+                        let openTime = new Date(element[0]);
+                        let closeTime = new Date(element[6]);
+                        element[0] = dataFormat.formatDate(openTime) + ' ' + dataFormat.formatTime(openTime);      
+                        element[6] = dataFormat.formatTime(closeTime);
+                        //element[11] = dataFormat.formatDate(openTime); 
+                        element.symbol = pairName; 
+                    });               
+               
+                    // Update dataPoint keys
+                    var dataList = dataToSave.map(function(dataPoint)
+                    { 
+                        return { 
+                            time: dataPoint[0], 
                             open: dataPoint[1],
                             high: dataPoint[2],
                             low: dataPoint[3],
@@ -298,18 +263,18 @@ program
             break;
             case 'poloniex':
                 console.log(chalk.cyan('     DATA: ')+ 'Parsing data...');
-                data.forEach(function(element) 
+                dataToSave.forEach(function(element) 
                 {   
                     let dateToMilSecs = element.date * 1000;
                     let openTime = new Date(dateToMilSecs);
                     element.time = dataFormat.formatDate(openTime) + " " + dataFormat.formatTime(openTime);
-                    element.symbol = pair;
+                    element.symbol = pairName;
                 });
-                dataList = data;        
+                dataList = dataToSave;        
             };
 
             //Write file
-            let fields = ['symbol', /*'date',*/ 'time','open', 'high', 'low', 'close', 'volume']
+            let fields = ['symbol', /*"TS",'date',*/ 'time','open', 'high', 'low', 'close', 'volume']
             var result = json2csv({ data: dataList, fields: fields });
            
             fs.writeFile(CSVfileName, result, function(err) 
@@ -318,7 +283,7 @@ program
                     console.log(err);
                 } else console.log(chalk.greenBright("     FILE SAVED AS: ") + CSVfileName);
             });  
-        
+            dataToSave = [];
         };
     });
       
--- test.js
+++ test.js
@@ -1,14 +1,5 @@
-function resolveAfter2Seconds(x) { 
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(x);
-        console.log("this");
-      }, 2000);
-    });
-  }
-  
-  async function f1() {
-    var x = await resolveAfter2Seconds(10);
-    console.log(x); // 10
-  }
-  f1();
+
+var symbol = 'BTC-ETH';
+var x = symbol.split("-");
+var pair = x[1] + x [0];
+console.log(pair)
